8b87c0392a099ab634861087a55f9efe10aa8c22
Jazy Llerena <jazy555@hotmail.com>
2016-03-09 10:13:28 -0500

    Complete excersise 3 and lab 8

---
+++ COP3502/Lab8/src/Counter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Counter {
+
+	public static void main(String[] args) throws InterruptedException { //Eww
+		Scanner in = new Scanner(System.in);
+		int secs, sec, min;
+		System.out.print("Enter time in seconds: ");
+		secs = in.nextInt();
+		//Now we loop for how ever many seconds
+		for (int i=0; i<=secs; i++) {
+			//Print the current time left
+			//Get the minutes left
+			min = (secs-i) / 60;
+			//Get the seconds left after minutes
+			sec = (secs-i) % 60;
+			//Print out the time
+			System.out.printf("%02d:%02d\n", min, sec);
+			//Now we take a nap
+			Thread.sleep(1000);
+		}
+		
+		in.close(); //Shh bby
+	}
+
+}
