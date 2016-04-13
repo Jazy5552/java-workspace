4bcc38c510ccf8a463bcb0dc2484ea08fcff3565
Jazy Llerena <jazy555@hotmail.com>
2016-04-13 18:00:16 -0400

    Insert data csv file

--- COP3502/Project4/src/InitialInventoryPopulator.java
+++ COP3502/Project4/src/InitialInventoryPopulator.java
@@ -1,10 +1,43 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class InventoryPopulator {
 
-		public static RecordStack getInitialInventory(String filename) {
-			//TODO Read file and create the appropriate records.
-			//TODO Create a record stack and add the records to it
+public class InitialInventoryPopulator {
+
+		/*
+		 * @params filename: Path to file containing inventory data
+		 * fortyFiveStack: RecordStack that will be filled with 45 type records
+		 * lpStack: RecordStack that will be filled with lp type records
+		 */
+		public static void getInitialInventory(String filename, RecordStack fortyFiveStack, RecordStack lpStack) {
+			//Read file and create the appropriate records.
+			//Create a record stack and add the records to it
+			String[] tmp;
+			Scanner in;
+			try {
+				in = new Scanner(new File(filename));
+			} catch (FileNotFoundException e) {
+				System.out.println(filename + " not found!");
+				return;
+			}
 			
-			return null;
+			while (in.hasNextLine()) {
+				//Split the string by commas
+				tmp = in.nextLine().split(", ");
+				//Artist, Title, Year, Type
+				if (tmp[3].equals("45")) {
+					//Create a forty five record
+					Record ffR = new FortyFive(tmp[0], tmp[1], Integer.parseInt(tmp[2]));
+					fortyFiveStack.push(ffR);
+				} else {
+					//tmp[3] == "lp"
+					//Create an lp record
+					Record lpR = new LP(tmp[0], tmp[1], Integer.parseInt(tmp[2]));
+					lpStack.push(lpR);
+				}
+			}
+			in.close();
+			//No return. We are doing it the C++ way of parameter references. ew
 		}
 }
--- COP3502/Project4/src/TestInitialInventoryPopulator.java
+++ COP3502/Project4/src/TestInitialInventoryPopulator.java
@@ -6,8 +6,10 @@ public class TestInitialInventoryPopulator {
 
 	@Test
 	public void test() {
-		RecordStack rs = InitialInventoryPopulator.getInitialInventory(
-				"/users/kristy/3502inclass/Lucy_Initial_Inventory.csv");
+		RecordStack rs = new RecordStack();
+		InitialInventoryPopulator.getInitialInventory(
+				"Lucy_Initial_Inventory.csv",
+				rs,rs);
 		assertEquals("Artist4",rs.pop().getArtist());
 		assertEquals("Title 3", rs.pop().getTitle());
 		assertEquals(2,rs.size());
@@ -18,8 +20,10 @@ public class TestInitialInventoryPopulator {
 	
 	@Test
 	public void testToString() {
-		RecordStack rs = InitialInventoryPopulator.getInitialInventory(
-				"/users/kristy/3502inclass/Lucy_Initial_Inventory.csv");
+		RecordStack rs = new RecordStack();
+		InitialInventoryPopulator.getInitialInventory(
+				"Lucy_Initial_Inventory.csv",
+				rs,rs);
 		assertEquals("LP: Title 4 by Artist4, 2004",
 				rs.pop().toString());
 		assertEquals("45: Title 3 by Artist3, 2003",
